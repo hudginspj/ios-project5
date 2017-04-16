1c5833c30d0c6d9772893ea92554c98ed54ca40b
Paul Hudgins <pjhudgins@pauls-macbook.local>
2017-04-16 00:47:43 -0400

    Initial Commit

---
+++ Assignment5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment5
+//
+//  Created by Paul Hudgins on 4/16/17.
+//  Copyright © 2017 Paul Hudgins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
