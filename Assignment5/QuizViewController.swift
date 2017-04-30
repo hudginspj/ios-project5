0cfec9533a85af18c8a66a05592c6ee206aae37e
William Nieporte <williamnieporte@Williams-Mac.local>
2017-04-30 19:43:15 -0400

    Added images

--- Assignment5/QuizViewController.swift
+++ Assignment5/QuizViewController.swift
@@ -20,10 +20,10 @@ class QuizViewController: UIViewController {
     @IBOutlet weak var P4Answer: UILabel!
     
     //Player score labels
-    @IBOutlet weak var P1Score: UILabel!
-    @IBOutlet weak var P2Score: UILabel!
-    @IBOutlet weak var P3Score: UILabel!
-    @IBOutlet weak var P4Score: UILabel!
+    @IBOutlet weak var P1ScoreLabel: UILabel!
+    @IBOutlet weak var P2ScoreLabel: UILabel!
+    @IBOutlet weak var P3ScoreLabel: UILabel!
+    @IBOutlet weak var P4ScoreLabel: UILabel!
     
     //Question stuff
     @IBOutlet weak var QuestionNumberLabel: UILabel!
@@ -37,23 +37,131 @@ class QuizViewController: UIViewController {
     
     //Quiz Answer choices
     @IBAction func AnswerA(sender: AnyObject) {
+        defaults.setValue("A", forKeyPath: "answer")
     }
     @IBAction func AnswerB(sender: AnyObject) {
+        defaults.setValue("B", forKeyPath: "answer")
     }
     @IBAction func AnswerC(sender: AnyObject) {
+        defaults.setValue("C", forKeyPath: "answer")
     }
     @IBAction func AnswerD(sender: AnyObject) {
+        defaults.setValue("D", forKeyPath: "answer")
     }
     
     //Other Stuff
     @IBOutlet weak var TimerLabel: UILabel!
     @IBAction func ResetQuiz(sender: AnyObject) {
+        score = 0
+        time = 25
+        timer = NSTimer()
+        questionNumber = 1
+        questionTotal = 2
+        P1Score=0
+        P2Score=0
+        P3Score=0
+        P4Score=0
+
+        
+        //Set initial values
+        defaults.setValue(nil, forKeyPath: "answer")
+        updateLabel(TimerLabel, text: String(25))
+        
+        //Set Answer labels and scores
+        updateLabel(P1Answer, text: "")
+        updateLabel(P2Answer, text: "")
+        updateLabel(P3Answer, text: "")
+        updateLabel(P4Answer, text: "")
+        updateLabel(P1ScoreLabel, text: String(P1Score))
+        updateLabel(P2ScoreLabel, text: String(P1Score))
+        updateLabel(P3ScoreLabel, text: String(P1Score))
+        updateLabel(P4ScoreLabel, text: String(P1Score))
+        
+        //Set question counter
+        updateLabel(QuestionNumberLabel, text: "Question: "+String(questionNumber)+"/"+String(questionTotal))
+        
+        
+        //TODO: Hide reset quiz button
+        ResetButton.alpha=0
+        ResetButton.enabled=false
+        
+        //TODO: Load Question 1
+
     }
+    @IBOutlet weak var ResetButton: UIButton!
+    
+    
+    //Variables
+    var score = 0
+    var time = 25
+    var timer = NSTimer()
+    var questionNumber = 1
+    var questionTotal = 2
+    var P1Score=0
+    var P2Score=0
+    var P3Score=0
+    var P4Score=0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Set initial values
+        defaults.setValue(nil, forKeyPath: "answer")
+        updateLabel(TimerLabel, text: String(25))
         
+        //Set Answer labels and scores
+        updateLabel(P1Answer, text: "")
+        updateLabel(P2Answer, text: "")
+        updateLabel(P3Answer, text: "")
+        updateLabel(P4Answer, text: "")
+        updateLabel(P1ScoreLabel, text: String(P1Score))
+        updateLabel(P2ScoreLabel, text: String(P1Score))
+        updateLabel(P3ScoreLabel, text: String(P1Score))
+        updateLabel(P4ScoreLabel, text: String(P1Score))
+        
+        //Set question counter
+        updateLabel(QuestionNumberLabel, text: "Question: "+String(questionNumber)+"/"+String(questionTotal))
+        
+        
+        //TODO: Hide reset quiz button
+        ResetButton.alpha=0
+        ResetButton.enabled=false
+        
+        //TODO: Load Question 1
+        
+        //Set Timer
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(QuizViewController.updateTimer), userInfo: nil, repeats: true)
+        
+        //TODO: Run check for correct answer for all players
+        
+        //TODO: Show correct answer for 3 seconds
+        
+        //TODO: Load next question
+        
+    }
+    
+    func updateTimer() {
+        time=time-1
+        if(time>=0){
+            updateLabel(TimerLabel, text: String(time))
+        }else if(time >= -3){
+            updateLabel(TimerLabel, text: "Times Up")
+        }else{
+            time=25
+            updateLabel(TimerLabel, text: String(time))
+            
+            if(questionNumber<questionTotal){
+                questionNumber=questionNumber+1
+                updateLabel(QuestionNumberLabel, text: "Question: "+String(questionNumber)+"/"+String(questionTotal))
+                
+                //Load next question
+            }
+            if(questionNumber==questionTotal){
+                //Show who won
+                ResetButton.alpha=100
+                ResetButton.enabled=true
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
